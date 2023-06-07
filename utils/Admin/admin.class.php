8f9ff9056d56b7688965680f567b944613b181fd
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-06-07 14:46:46 +0530

    Login Logic Created

---
+++ utils/Admin/admin.class.php
@@ -0,0 +1,55 @@
+<?php
+
+    //Admin Class to define all functions related to Admin
+
+
+    Class Admin{
+
+        public $email;
+        public $fullName;
+        public $deptID;
+        public $joiningDate;
+        public $deactivationDate;
+        public $type;
+        public $role;
+        public $status;
+
+         /*
+        @function "__contruct"
+        @description "Contructor 
+                        --> get user data and set values
+                        
+         */
+        public function __construct( $employeeID ){
+
+            //Establish Connection
+            $conn = sql_conn();
+            
+            //Run a Query
+            $getUserInfo = "Select * from employees where employeeID = $employeeID";
+            $result =  mysqli_query( $conn , $getUserInfo);
+
+            // Error Handling
+            if ( !$result ) {
+                echo("Error description: " . mysqli_error($con));
+                return false;
+            }
+
+            $row = mysqli_fetch_assoc($result); // Response
+
+            $this->email = $row['email'];
+            $this->fullName = $row['fullName'];
+            $this->deptID = $row['deptID'];
+            $this->joiningDate = $row['joiningDate'];
+            $this->deactivationDate = $row['deactivationDate'];
+            $this->type = $row['type'];
+            $this->role = $row['role'];
+            $this->status = $row['status'];
+
+        }
+
+
+    }
+
+
+?>
