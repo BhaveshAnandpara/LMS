97d82461fad6d2504e5e6cfc24ef38591b28e089
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-06-10 21:17:28 +0530

    dasboard changed

--- pages/Admin/dashboard.php
+++ pages/Admin/dashboard.php
@@ -33,6 +33,9 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../../css/dashboard.css">
+
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
@@ -48,125 +51,137 @@
 
 <body>
 
-<?php 
-include "../../includes/Staff/sidenavbar.php";
-?>
-    
+    <?php include "../../includes/Admin/sidenavbar.php"  ?>
 
     <section class="home-section">
 
-        <div class="container bg-white rounded-lg shadow-lg mt-3">
-            <div class="row p-3 rounded-lg shadow-lg d-flex justify-content-sm-center "
-                style="transition: all all 0.5s ease; border-right:6px solid #11101D; ">
-                <div class="col-md-3 col-sm-12  col-sm-12 rounded-lg m-3 bg-white shadow-lg"
-                    style="border-right:6px solid #11101D ">
+        <div class="cards-container container-fluid bg-white row p-3 rounded-lg shadow-lg d-flex justify-content-sm-center  " style="transition: all all 0.5s ease; border-right:6px solid #11101D" >
 
-                    <div class="row p-2 pr-0">
-                        <div class="col-12  ">
-                            <div class=" row pb-1 pl-2 d-flex justify-content-sm-center">
-                                <h5>Total Employees</h5>
-                            </div>
-                            <div class="row d-flex justify-content-sm-center">
-                                <h5> <?php echo  "<p class='stats' >" . $user->getTotalEmployee() . "</p>" ?>
-                                </h5>
+            <div class="header-body">
+                <div class="row">
+
+                    <!-- Card -->
+                    <div class=" card-width  col-lg-6">
+                        <div class="card card-stats mb-4 mb-xl-0">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <h5 class="card-title text-uppercase text-muted mb-3">Total Employees</h5>
+                                        <span class="h2 font-weight-bold mb-0"> <?php echo $user->getTotalEmployee() ?>
+                                        </span>
+                                    </div>
+
+                                </div>
+                                <p class=" no-desc mt-3 mb-0 text-muted text-sm">
+                                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                                    <span class="text-nowrap">Since last month</span>
+                                </p>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-3 col-sm-12  col-sm-12 rounded-lg m-3 bg-white shadow-lg"
-                    style="border-right:6px solid #11101D ">
-                    <div class="row p-2 pr-0">
 
-                        <div class="col-12  ">
-                            <div class=" row pb-1 pl-2 d-flex justify-content-sm-center">
-                                <h5> Total Department</h5>
-                            </div>
-                            <div class="row d-flex justify-content-sm-center">
-                                <h5> <?php echo  "<p class='stats' >" . $user->getTotalDepartment() . "</p>" ?>
-                                </h5>
+                    <!-- Card -->
+                    <div class=" card-width  col-lg-6">
+                        <div class="card card-stats mb-4 mb-xl-0">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <h5 class="card-title text-uppercase text-muted mb-3">Total Employees</h5>
+                                        <span class="h2 font-weight-bold mb-0"> <?php echo $user->getTotalEmployee() ?>
+                                        </span>
+                                    </div>
+
+                                </div>
+                                <p class=" no-desc mt-3 mb-0 text-muted text-sm">
+                                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                                    <span class="text-nowrap">Since last month</span>
+                                </p>
                             </div>
                         </div>
                     </div>
-                    <div class="row border-top p-3">
-                        hello
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-12  col-sm-12 rounded-lg m-3 bg-white shadow-lg"
-                    style="border-right:6px solid #11101D ">
-                    <div class="row p-2 pr-0">
 
-                        <div class="col-12  ">
-                            <div class=" row pb-1 pl-2 d-flex justify-content-sm-center">
-                                <h5> Total Leave Type</h5>
-                            </div>
-                            <div class="row d-flex justify-content-sm-center">
-                                <h5> <?php echo  "<p class='stats' >" . $user->getTotalLeaveTypes() . "</p>" ?>
-                                </h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row border-top p-3">
-                        hello
-                    </div>
-                </div>
+                    <!-- Card -->
+                    <div class=" card-width  col-lg-6">
+                        <div class="card card-stats mb-4 mb-xl-0">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <h5 class="card-title text-uppercase text-muted mb-3">Total Employees</h5>
+                                        <span class="h2 font-weight-bold mb-0"> <?php echo $user->getTotalEmployee() ?>
+                                        </span>
+                                    </div>
 
-                <div class="col-md-3 col-sm-12  col-sm-12 rounded-lg m-3 bg-white shadow-lg"
-                    style="border-right:6px solid #11101D ">
-                    <div class="row p-2 pr-0">
-
-                        <div class="col-12  ">
-                            <div class=" row pb-1 pl-2 d-flex justify-content-sm-center">
-                                <h5> Teaching Staff</h5>
-                            </div>
-                            <div class="row d-flex justify-content-sm-center">
-                                <h5> <?php echo  "<p class='stats' >" . $user->getTotalTeachingStaff() . "</p>" ?>
-                                </h5>
+                                </div>
+                                <p class=" no-desc mt-3 mb-0 text-muted text-sm">
+                                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                                    <span class="text-nowrap">Since last month</span>
+                                </p>
                             </div>
                         </div>
                     </div>
-                    <div class="row border-top p-3">
-                        hello
-                    </div>
-                </div>
 
-                <div class="col-md-3 col-sm-12  col-sm-12 rounded-lg m-3 bg-white shadow-lg"
-                    style="border-right:6px solid #11101D ">
-                    <div class="row p-2 pr-0">
+                    <!-- Card -->
+                    <div class=" card-width  col-lg-6">
+                        <div class="card card-stats mb-4 mb-xl-0">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <h5 class="card-title text-uppercase text-muted mb-3">Total Employees</h5>
+                                        <span class="h2 font-weight-bold mb-0"> <?php echo $user->getTotalEmployee() ?>
+                                        </span>
+                                    </div>
 
-                        <div class="col-12  ">
-                            <div class=" row pb-1 pl-2 d-flex justify-content-sm-center">
-                                <h5> Non-Teaching Staff</h5>
-                            </div>
-                            <div class="row d-flex justify-content-sm-center">
-                                <h5> <?php echo  "<p class='stats' >" . $user->getTotalNonTeachingStaff() . "</p>" ?>
-                                </h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row border-top p-3">
-                        hello
-                    </div>
-                </div>
-
-                <div class="col-md-3 col-sm-12  col-sm-12 rounded-lg m-3 bg-white shadow-lg"
-                    style="border-right:6px solid #11101D ">
-                    <div class="row p-2 pr-0">
-
-                        <div class="col-12  ">
-                            <div class=" row pb-1 pl-2 d-flex justify-content-sm-center">
-                                <h5> Deactivated Staff</h5>
-                            </div>
-                            <div class="row d-flex justify-content-sm-center">
-                                <h5> <?php echo  "<p class='stats' >" . $user->getTotalInactiveStaff() . "</p>" ?>
-                                </h5>
+                                </div>
+                                <p class=" no-desc mt-3 mb-0 text-muted text-sm">
+                                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                                    <span class="text-nowrap">Since last month</span>
+                                </p>
                             </div>
                         </div>
                     </div>
-                    <div class="row border-top p-3">
-                        hello
-                    </div>
-                </div>
 
+                    <!-- Card -->
+                    <div class=" card-width  col-lg-6">
+                        <div class="card card-stats mb-4 mb-xl-0">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <h5 class="card-title text-uppercase text-muted mb-3">Total Employees</h5>
+                                        <span class="h2 font-weight-bold mb-0"> <?php echo $user->getTotalEmployee() ?>
+                                        </span>
+                                    </div>
+
+                                </div>
+                                <p class=" no-desc mt-3 mb-0 text-muted text-sm">
+                                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                                    <span class="text-nowrap">Since last month</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Card -->
+                    <div class=" card-width  col-lg-6">
+                        <div class="card card-stats mb-4 mb-xl-0">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <h5 class="card-title text-uppercase text-muted mb-3">Total Employees</h5>
+                                        <span class="h2 font-weight-bold mb-0"> <?php echo $user->getTotalEmployee() ?>
+                                        </span>
+                                    </div>
+
+                                </div>
+                                <p class=" no-desc mt-3 mb-0 text-muted text-sm">
+                                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                                    <span class="text-nowrap">Since last month</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
             </div>
         </div>
 
@@ -180,7 +195,7 @@ include "../../includes/Staff/sidenavbar.php";
                   
             ?>
 
-            <p> <?php echo $value ?> </p>
+        <p> <?php echo $value ?> </p>
 
         <?php } ?>
         </p>
