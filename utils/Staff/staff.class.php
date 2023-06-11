055e5503872422e62f409a48df82cb47eaf36e71
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-06-11 18:57:29 +0530

    Bug Solved for faculty Login

--- utils/Staff/staff.class.php
+++ utils/Staff/staff.class.php
@@ -2,16 +2,48 @@
 
     Class Staff{
 
-        //Methods will written over here
-        // e.g methods for staff apply ( same for both HOD and staff )
+        public $employeeId;
+        public $email;
+        public $fullName;
+        public $deptID;
+        public $joiningDate;
+        public $deactivationDate;
+        public $type;
+        public $role;
+        public $status;
 
-        /**
-         * Class constructor.
+         /*
+        @function "__contruct"
+        @description "Contructor 
+                        --> get user data and set values
+                        
          */
-        public function __construct( $role )
-        {
+        public function __construct( $employeeID ){
+
+            //Establish Connection
+            $conn = sql_conn();
             
-        
+            //Run a Query
+            $getUserInfo = "Select * from employees where employeeID = $employeeID";
+            $result =  mysqli_query( $conn , $getUserInfo);
+
+            // Error Handling
+            if ( !$result ) {
+                echo("Error description: " . mysqli_error($conn));
+                return false;
+            }
+
+            $row = mysqli_fetch_assoc($result); // Response
+
+            $this->employeeId = $row['employeeID'];
+            $this->email = $row['email'];
+            $this->fullName = $row['fullName'];
+            $this->deptID = $row['deptID'];
+            $this->joiningDate = $row['joiningDate'];
+            $this->deactivationDate = $row['deactivationDate'];
+            $this->type = $row['type'];
+            $this->role = $row['role'];
+            $this->status = $row['status'];
 
         }
 
@@ -28,9 +60,7 @@
 
     Class Faculty extends Staff{
 
-        //Methods will written over here 
-        // e.g methods for staff apply ( same for both HOD and staff )
-
+        
 
     }
 
