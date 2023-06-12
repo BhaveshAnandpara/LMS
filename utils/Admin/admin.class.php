9eb08ff3082016a0db4fe8bd9a0f80aa35873afc
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-06-12 17:56:08 +0530

    Admin Dashboard Bug Solved

--- utils/Admin/admin.class.php
+++ utils/Admin/admin.class.php
@@ -54,7 +54,154 @@
 
         // ------------------------------------ Dashboard ------------------------------------ //
 
-         
+           /*
+        @function "getTotalEmployee"
+        @description "getTotalEmployee
+                        --> get the count of total Active employees 
+                        
+         */
+
+        public function getTotalEmployee(){
+
+            $ACTIVE = Config::$_EMPLOYEE_STATUS['ACTIVE'] ;
+
+            // SQL Query to get the count of all active employees
+            $sql = "SELECT COUNT(employeeID) from employees Where status='$ACTIVE' ";
+            $conn = sql_conn();
+            $result =  mysqli_query( $conn , $sql);
+
+            if( !$result ) echo("Error description: " . mysqli_error($con));
+
+            $rows = mysqli_fetch_assoc($result); // Response
+
+            return $rows['COUNT(employeeID)'];
+
+        }
+
+                 /*
+        @function "getTotalDepartment"
+        @description "getTotalDepartment
+                        --> get the count of total Departments
+                        
+         */
+
+         public function getTotalDepartment(){
+
+            // SQL Query to get the count of all departments
+            $sql = "SELECT COUNT(deptID) from departments";
+            $conn = sql_conn();
+            $result =  mysqli_query( $conn , $sql);
+
+            if( !$result ) echo("Error description: " . mysqli_error($con));
+
+            $rows = mysqli_fetch_assoc($result); // Response
+
+            return $rows['COUNT(deptID)'];
+
+        }
+
+
+        /*
+        @function "getTotalLeaveTypes"
+        @description "getTotalLeaveTypes
+                        --> get the count of total Leave Types
+                        
+         */
+
+         public function getTotalLeaveTypes(){
+
+            $status = Config::$_MASTERADTA_STATUS['ACTIVE'] ;
+
+            // SQL Query to get the count of all leave types
+            $sql = "SELECT COUNT(leaveID) from masterdata where status='$status' ";
+            $conn = sql_conn();
+            $result =  mysqli_query( $conn , $sql);
+
+            if( !$result ) echo("Error description: " . mysqli_error($con));
+
+            $rows = mysqli_fetch_assoc($result); // Response
+
+            return $rows['COUNT(leaveID)'];
+
+        }
+
+                /*
+        @function "getTotalTeachingStaff"
+        @description "getTotalTeachingStaff
+                        --> get the count of total Teaching Staff
+                        
+         */
+
+         public function getTotalTeachingStaff(){
+
+            $status = Config::$_MASTERADTA_STATUS['ACTIVE'] ;
+            $type = Config::$_EMPLOYEE_TYPE['TEACHING_STAFF'] ;
+
+            // SQL Query to get the count of all teaching staff
+            $sql = "SELECT COUNT(employeeID) from employees where status='$status' and type='$type' ";
+            $conn = sql_conn();
+            $result =  mysqli_query( $conn , $sql);
+
+            if( !$result ) echo("Error description: " . mysqli_error($con));
+
+            $rows = mysqli_fetch_assoc($result); // Response
+
+            return $rows['COUNT(employeeID)'];
+
+        }
+
+
+                        /*
+        @function "getTotalNonTeachingStaff"
+        @description "getTotalNonTeachingStaff
+                        --> get the count of total Non Teaching Staff
+                        
+         */
+
+         public function getTotalNonTeachingStaff(){
+
+            $status = Config::$_MASTERADTA_STATUS['ACTIVE'] ;
+            $type = Config::$_EMPLOYEE_TYPE['NON_TEACHING_STAFF'] ;
+
+            // SQL Query to get the count of all non teaching staff
+            $sql = "SELECT COUNT(employeeID) from employees where status='$status' and type='$type' ";
+            $conn = sql_conn();
+            $result =  mysqli_query( $conn , $sql);
+
+            if( !$result ) echo("Error description: " . mysqli_error($con));
+
+            $rows = mysqli_fetch_assoc($result); // Response
+
+            return $rows['COUNT(employeeID)'];
+
+        }
+
+
+                                /*
+        @function "getTotalDeactivatedStaff"
+        @description "getTotalDeactivatedStaff
+                        --> get the count of Total Deactivated Staff
+                        
+         */
+
+         public function getTotalInactiveStaff(){
+
+            $status = Config::$_MASTERADTA_STATUS['INACTIVE'] ;
+
+            // SQL Query to get the count of all inactive employees
+            $sql = "SELECT COUNT(employeeID) from employees where status='$status' ";
+            $conn = sql_conn();
+            $result =  mysqli_query( $conn , $sql);
+
+            if( !$result ) echo("Error description: " . mysqli_error($con));
+
+            $rows = mysqli_fetch_assoc($result); // Response
+
+            return $rows['COUNT(employeeID)'];
+
+        }
+
+
         
         /*
         @function "getNotifications"
